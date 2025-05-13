e0065472c6a5702f0044874e851a1213eaafd1a6
yogesh <yogirao8440@gmail.com>
2025-05-13 10:53:58 +0530

    dev: swagger autogen for dev purpose

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My API',
+    description: 'Description'
+  },
+  host: 'localhost:5000'
+};
+
+const outputFile = './swagger-output.json';
+const routes = ['./index.js'];
+
+
+
+swaggerAutogen(outputFile, routes, doc);
